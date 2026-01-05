b7ae1e3b9a39452e64245ab889390d6ac5a6edec
hush006 <alameenbusari70@gmail.com>
2026-01-05 11:32:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const min = 0;
+const max = 100;
+const answer = Math.floor(Math.random() * (max - min + 1));
+//console.log(answer)
+
+let guess;
+let running = true;
+let attempts = 0;
+
+
+
+while(running){
+    guess = window.prompt(`Guess a number between ${min} - ${max}`);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+     window.alert('please enter a valid number');
+       
+    }
+    if(guess==answer){
+        alert(`Your value is correct with ${attempts} total attempts`) 
+        break;       
+    }else if(guess>answer){
+         window.alert('TOO HIGH, TRY AGAIN!');
+         attempts++
+       
+    }else{
+         window.alert('TOO LOW, TRY AGAIN!');
+         attempts++
+    }
+   
+}
+
+
+
+// let city = 'lagos';
+// let country = 'Ngeria';
+// const geoLocation = city + ',' + ' ' + country;
+
+// console.log(geoLocation)
